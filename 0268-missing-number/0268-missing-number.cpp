d22c324c677836b23689b07fd30640230178843b
pratham grover <85804968+PrathamLearnsToCode@users.noreply.github.com>
2024-01-21 09:06:30 +0530

    Time: 7 ms (98.41%), Space: 20.3 MB (5.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,12 @@
-#include<vector>
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        std::vector<int> arr(nums.size() + 1, 0);
+        int n = nums.size();
+        int sum = (n*(n+1))/2;
+        int s = 0;
         for(int i = 0; i<nums.size(); i++){
-            arr[nums[i]] = 1;
+            s = nums[i] + s;
         }
-        
-        for(int j = 0; j<arr.size(); j++){
-            if(arr[j]==0){
-                return j;
-            }
-        }
-        
-        return -1;   
+        return (sum-s);
     }
 };
