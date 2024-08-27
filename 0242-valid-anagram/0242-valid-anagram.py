fd14e367b24b58013bdc748a909fe55d967fc44e
pratham grover <85804968+PrathamLearnsToCode@users.noreply.github.com>
2024-08-27 17:51:12 +0530

    Time: 42 ms (86.54%), Space: 17.1 MB (27.30%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        countS = {}
+        countT = {}
         
-        countS ,countT = {} , {}
+        for i in list(s):
+            if i in countS:
+                countS[i] += 1
+            else:
+                countS[i] = 1
+                
+        for j in list(t):
+            if j in countT:
+                countT[j] += 1
+            else:
+                countT[j] = 1
+                
+        return countS==countT
+            
         
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
-            
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
-            
-        return True
